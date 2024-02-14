82047133b65422dc30a3756d4962d86c51059f10
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-14 03:30:36 +0200

    (Try the sys molule) Complete

---
+++ ex13.py
@@ -0,0 +1,8 @@
+# import from sys the argv module
+from sys import argv
+
+# Those args input from terminal when you run the script
+script, x, y, z = argv
+
+# And print the outputs
+print(f'Those Variables is type when you run the script {script}, {x} {y} {z}')
